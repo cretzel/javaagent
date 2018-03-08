c84b2d5e6a9ac4921366b2bdc6bcfa9aa0e1772d
Nick Wiedenbrueck <nick.wiedenbrueck@bertelsmann.de>
2018-03-08 22:44:04 +0100

    Sugar

--- javaagent-agent/javaagent-bytebuddy/src/main/java/de/nick/agent/FetzedInterceptor.java
+++ javaagent-agent/javaagent-bytebuddy/src/main/java/de/nick/agent/FetzedInterceptor.java
@@ -7,7 +7,6 @@ import net.bytebuddy.implementation.bind.annotation.SuperCall;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -23,9 +22,12 @@ public class FetzedInterceptor {
                                    @Origin Method method, @Origin Class clazz) throws Exception {
 
         final List<Annotation> annotations = Arrays.asList(method.getAnnotations());
-        final Optional<Annotation> fetzedAnnotation = annotations.stream().filter(a -> a.annotationType().getName().equals("de.nick.agenttest.annotation.Fetzed")).findAny();
-        if (fetzedAnnotation.isPresent()) {
-            final String dude = (String) fetzedAnnotation.get().annotationType().getDeclaredMethod("dude").invoke(fetzedAnnotation.get());
+        final Optional<Annotation> fetzedAnnotationOpt = annotations.stream()
+            .filter(a -> a.annotationType().getName().equals("de.nick.agenttest.annotation.Fetzed"))
+            .findAny();
+        if (fetzedAnnotationOpt.isPresent()) {
+            final Annotation fetzedAnnotation = fetzedAnnotationOpt.get();
+            final String dude = (String) fetzedAnnotation.annotationType().getDeclaredMethod("dude").invoke(fetzedAnnotation);
             System.out.println(method.getName() + " is fetzed by " + dude);
         }
         return callable.call();
